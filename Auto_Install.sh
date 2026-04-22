84a10a2446847cdc2ac51f2aef36bb970aa501c5
DS1UYM <sejung84@gmail.com>
2026-04-22 11:55:07 +0900

    Enhance Auto_Install.sh with installation banner
    Added installation banner and initial messages to the script.

--- Auto_Install.sh
+++ Auto_Install.sh
@@ -1,3 +1,21 @@
+clear
+cat << 'EOF'
+===========================================================
+ _____  __      __  _____   _   _   __  __   _____ 
+|  __ \ \ \    / / / ____| | \ | | |  \/  | / ____|
+| |  | | \ \  / / | (___   |  \| | | \  / || (___  
+| |  | |  \ \/ /   \___ \  | . ` | | |\/| | \___ \ 
+| |__| |   \  /    ____) | | |\  | | |  | | ____) |
+|_____/     \/    |_____/  |_| \_| |_|  |_||_____/ 
+
+          dvsNMS All-in-One Package Installation
+          Copyright (c) DS1UYM Sejung Kim
+===========================================================
+EOF
+
+echo -e "\n설치를 시작합니다. 잠시만 기다려주세요...\n"
+sleep 3
+
 cd /tmp && \
 wget -O step0_setup https://raw.githubusercontent.com/ds1uym/DVSMU-PHP-NMS/main/Step0_grubzero.sh && \
 sudo chmod +x ./step0_setup && \
