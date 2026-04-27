3dcd6040bf56c716f82ec81d188d080ffb3bf9c3
DS1UYM <sejung84@gmail.com>
2026-04-28 05:36:41 +0900

    Ensure alias for dvs is added to .bashrc only once

--- Auto_Install.sh
+++ Auto_Install.sh
@@ -27,7 +27,7 @@ sudo chmod +x trixie && \
 sudo ./trixie && \
 sudo apt-get update && \
 sudo apt-get install dvswitch-server -y && \
-echo "alias dvs='sudo /usr/local/dvs/dvs'" >> ~/.bashrc && \
+grep -qxF "alias dvs='sudo /usr/local/dvs/dvs'" ~/.bashrc || echo "alias dvs='sudo /usr/local/dvs/dvs'" >> ~/.bashrc && \
 wget -O step2_setup https://raw.githubusercontent.com/ds1uym/DVSMU-PHP-NMS/main/Step2_NMS_Setup.run && \
 sudo chmod +x ./step2_setup && \
 sudo ./step2_setup && \
